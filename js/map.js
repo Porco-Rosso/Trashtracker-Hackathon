1cdb18bbc8f6032fbd30d709255569f29599e4c6
eArraez <arraez.elena@gmail.com>
2016-10-01 12:42:41 +0200

    ..

--- js/map.js
+++ js/map.js
@@ -11,8 +11,8 @@ $(function () {
 					panControl: false,
 					mapTypeId: google.maps.MapTypeId.ROADMAP
 				};
-				var map = new google.maps.Map(mapCanvas, mapOptions);
-
+				
+                var map = new google.maps.Map(mapCanvas, mapOptions);
 
 				var truck = new google.maps.Marker({
 				position: maporigin,
@@ -20,11 +20,11 @@ $(function () {
 				icon: 'http://maps.google.com/mapfiles/ms/icons/truck.png'
                 });
         
+
+
                 var infowindow = new google.maps.InfoWindow();        
                 var marker, i;
-
-				for (i = 0; i < placemarkers.length; i++) {
-                    
+				for (i = 0; i < placemarkers.length; i++) {                   
                     //var marker_name  = placemarkers[i][0];
                     //var marker_lat   = placemarkers[i][1];
                     //var marker_lng   = placemarkers[i][2];
@@ -35,15 +35,17 @@ $(function () {
 						map: map
 					});
 				
-                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        infowindow.setContent(placemarkers[i][0]+ '<br/>' + placemarkers[i][3]);
-                        infowindow.open(map, marker);
-                    }
+                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(placemarkers[i][0]+ '<br/>' + placemarkers[i][3]);
+                            infowindow.open(map, marker);
+                        }
+                    
+                        })(marker, i));
                 
-                    })(marker, i));
-                
-                }	
-        }
+                }
+
+            }
+
 		google.maps.event.addDomListener(window, 'load', initMap);
 });
--- js/truck.js
+++ js/truck.js
@@ -1,24 +1,25 @@
 $(function () {
 
-			function truck() {
-
-				var maporigin = new google.maps.LatLng(40.4379571, -3.6896047);
-
-				var mapCanvas = document.getElementById('map');
-				var mapOptions = {
-					center: maporigin,
-					zoom: 16,
-					panControl: false,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				};
-				var map = new google.maps.Map(mapCanvas, mapOptions);
-
-				var marker = new google.maps.Marker({
-					position: maporigin,
-					map: map,
-					icon: 'http://maps.google.com/mapfiles/ms/icons/truck.png'
-            		//animation: google.maps.Animation.DROP
-				});
-     }   
-              
+  function calcRoute() {
+  	 var directionDisplay;
+      var directionsService = new google.maps.DirectionsService(); 
+                directionsDisplay = new google.maps.DirectionsRenderer();
+                directionsDisplay.setMap(map);
+                directionsDisplay.setPanel(document.getElementById("directionsPanel"));	
+      var start = document.getElementById("routeStart").value;
+      var end = "40.437660, -3.681622";
+      var request = {
+        origin:start,
+        destination:end,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        }
+      });
+    }
+	
+				
+                      
 });
