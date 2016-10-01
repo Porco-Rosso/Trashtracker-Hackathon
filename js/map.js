9fc1784e3e41101bae10684aff5f669190b4ca5d
eArraez <arraez.elena@gmail.com>
2016-10-01 11:08:44 +0200

    updated map.js

--- js/map.js
+++ js/map.js
@@ -19,23 +19,29 @@ $(function () {
 					map: map,
 				});
         
-        var infowindow = new google.maps.InfoWindow();        
-        var marker, i;
+                var infowindow = new google.maps.InfoWindow();        
+                var marker, i;
 
 				for (i = 0; i < placemarkers.length; i++) {
+                    
+                    var marker_name  = placemarkers[i][0];
+                    var marker_lat   = placemarkers[i][1];
+                    var marker_lng   = placemarkers[i][2];
+                    var marker_state = placemarkers[i][3];
+
 					marker = new google.maps.Marker({
-						position: new google.maps.LatLng(placemarkers[i][1], placemarkers[i][2]),
+						position: new google.maps.LatLng(marker_lat, marker_lng),
 						map: map
 					});
-				}
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent(marcadores[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-      }
 				
-
-				google.maps.event.addDomListener(window, 'load', initMap);
-			});
+                google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                    return function() {
+                        infowindow.setContent(marker_name + '<br/>' + marker_state);
+                        //infowindow.setContent(marker_state);
+                        infowindow.open(map, marker);
+                    }
+                })(marker, i));
+                }	
+        }
+		google.maps.event.addDomListener(window, 'load', initMap);
+});
